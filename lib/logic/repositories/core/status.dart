a17c6406f533f56829ad027c305b5fbb00a23f3f
Iurii <y.luchaninov@gmail.com>
2022-11-28 08:49:26 +0100

    polishing

--- lib/logic/repositories/core/status.dart
+++ lib/logic/repositories/core/status.dart
@@ -5,19 +5,22 @@ class Status {
   static final _statuses = <int, Object?>{};
   static final _core = BehaviorSubject<Map<int, Object?>>();
 
+  static int _key(Object object) =>Object.hash(object, '');
+
   static void update<T>(Object status) {
-    _statuses[Object.hash(T, '')] = status;
+    assert(T != dynamic, 'You need to setup Repository Class');
+    _statuses[_key(T)] = status;
     _core.sink.add(_statuses);
   }
 
-  static void clear<T>() {
-    _statuses[Object.hash(T, '')] = null;
+  static void clear() {
+    _statuses.clear();
   }
 
   static Stream<Object> stream<T>() => _core.stream.transform(
       StreamTransformer<Map<int, Object?>, Object>.fromHandlers(
           handleData: (Map<int, Object?> data, EventSink sink) =>
-              sink.add(data[Object.hash(T, '')])));
+              sink.add(data[_key(T)])));
 
   static Future<dynamic> dispose() => _core.close();
 }
